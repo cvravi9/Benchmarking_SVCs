686340e52d0aa3994a28c160dc95d7c7eea5508e
cvravi9 <cvravi9@gmail.com>
2021-01-08 16:08:07 +0100

    Adding modified code for allele frequency comparision.

--- Capture_Regions_Comparision/Miracum_Capture_Region_Comparisions/Edit.py
+++ Capture_Regions_Comparision/Miracum_Capture_Region_Comparisions/Edit.py
@@ -1,12 +1,29 @@
 import pandas as pd
 
-First_Input = pd.read_csv("Miracum_0.4_Allele_Frequency.frq")
-First_Input.to_csv('First_Input.csv', index = None)
+dff = pd.read_csv("Miracum_0.4_Allele_Frequency.frq", sep = '\t', index_col= False)
+dff.columns = ['CHROM', 'POS', 'N_ALLELES', 'N_CHR', 'ALLELE:FREQ']
+dff["CHROM-POS"] = dff['CHROM'].astype(str) + '-' + dff['POS'].astype(str)
 
-df = pd.read_csv('First_Input.csv', sep='\t')
-First_Input_Edit = pd.concat(df, keys=["CHROM", "POS"])
-df1 = df['CHROM'].map(str) + '-' + df['POS'].map(str)
-print(df1)
 
-# dff = df.drop(['N_ALLELES', 'N_CHR', 'N_ALLELES.1', 'N_CHR.1', 'CHROM.1', 'POS.1', 'Unnamed: 5'], axis=1)
-# print(dff)
+dff = dff.drop(['N_ALLELES', 'N_CHR', 'CHROM', 'POS'], axis=1)
+cols = dff.columns.tolist()
+cols = cols[-1:] + cols[:-1]
+dff = dff[cols]
+print(dff)
+
+dff1 = pd.read_csv("Miracum_0.7_Allele_Frequency.frq", sep = '\t', index_col= False)
+dff1.columns = ['CHROM', 'POS', 'N_ALLELES', 'N_CHR', 'ALLELE:FREQ']
+dff1["CHROM-POS"] = dff1['CHROM'].astype(str) + '-' + dff1['POS'].astype(str)
+
+
+dff1 = dff1.drop(['N_ALLELES', 'N_CHR', 'CHROM', 'POS'], axis=1)
+cols = dff1.columns.tolist()
+cols = cols[-1:] + cols[:-1]
+dff1 = dff1[cols]
+print(dff1)
+
+result = pd.merge(dff, dff1, on="CHROM-POS")
+result.columns = ['CHROM-POS', 'AF-0.4', 'AF-0.7']
+print(result)
+
+result.to_csv('Miracum_AF_Comparision.csv', index = None)
