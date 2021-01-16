01d0b17aa424a4f0bbbec05b685702d433e26f3a
cvravi9 <cvravi9@gmail.com>
2021-01-16 21:42:22 +0100

    Adding plots as pdf.

--- Capture_Regions_Comparision/Miracum_Capture_Region_Comparisions/Site_Depth/Miracum_0.4_Depth_Normal_Plot.py
+++ Capture_Regions_Comparision/Miracum_Capture_Region_Comparisions/Site_Depth/Miracum_0.4_Depth_Normal_Plot.py
@@ -10,16 +10,12 @@ df = pd.read_csv("Miracum_0.4_DP.csv", sep= "\t", names=column_names)
 # Converting the column into a list.
 CHROM_POS_List = df.CHROM_POS.to_list()
 NORMAL_List = df.NORMAL_DP.to_list()
-TUMOR_List = df.TUMOR_DP.to_list()
 
 # Printing the list.
 print(CHROM_POS_List)
 print(NORMAL_List)
-print(TUMOR_List)
 
 # Showing the output and saving it into a PDF.
 plt.plot(CHROM_POS_List, NORMAL_List, label='Position vs Normal Depth')
-plt.plot(CHROM_POS_List, TUMOR_List, label='Postion vs Tumor Depth')
 plt.show()
 plt.savefig('Miracum_0.4_Normal_Plot.pdf')
-plt.savefig('Miracum_0.4_Tumor_Plot.pdf')
