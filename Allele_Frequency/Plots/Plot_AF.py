250c8042299ee7359d03d0c9c5d9d58d679fb44b
cvravi9 <cvravi9@gmail.com>
2021-02-02 17:03:09 +0100

    Renaming files.

--- Allele_Frequency/Plots/Plot_AF.py
+++ Allele_Frequency/Plots/Plot_AF.py
@@ -1,14 +1,21 @@
-# Importing packages.
-import numpy as np
+# Importing the needed packages.
+import matplotlib.pyplot as plt
 import pandas as pd
+import csv
 
-# Importing frq files and assigning columns.
-dff = pd.read_csv("Somatic_0.7_AF.frq", sep = '\t', index_col= False)
-dff.columns = ['CHROM', 'POS', 'N_ALLELES', 'N_CHR', 'ALLELE:FREQ']
+# Mentioning the column names and inputing the csv file.
+column_names = ["POS", "ALLELE"]
+df = pd.read_csv("Somatic_0.4_AF_Plot.csv", sep= "\t", names=column_names)
 
-# Deleting the unneeded columns.
-dff = dff.drop(['N_ALLELES', 'N_CHR', 'CHROM'], axis=1)
-print(dff)
+# Converting the column into a list.
+POS_List = df.POS.to_list()
+FREQ_List = df.ALLELE.to_list()
 
-# Saving the outcome to csv file.
-dff.to_csv('Somatic_0.7_AF_Plot.csv', sep='\t', index = None)
+# Printing the list.
+print(POS_List)
+print(FREQ_List)
+
+# Showing the output and saving it into a PDF.
+plt.plot(POS_List, FREQ_List, label='Postions vs Allele Frequency')
+plt.show()
+plt.savefig('Somatic_0.4_Plot.pdf')
