96e6dbe2d8205990a196fc5fa8eb3d09464c0422
cvravi9 <cvravi9@gmail.com>
2021-02-05 22:37:52 +0100

    Reorganising files.

---
+++ Allele_Frequency/VarScan/Plots/Chr/VarScan/Somatic_0.4_Chr/CSV_AF.py
@@ -0,0 +1,14 @@
+# Importing packages.
+import numpy as np
+import pandas as pd
+
+# Importing frq files and assigning columns.
+dff = pd.read_csv("Somatic_0.4_Chr17.frq", sep = '\t', index_col= False)
+dff.columns = ['CHROM', 'POS', 'N_ALLELES', 'N_CHR', 'ALLELE:FREQ']
+
+# Deleting the unneeded columns.
+dff = dff.drop(['N_ALLELES', 'N_CHR', 'CHROM'], axis=1)
+print(dff)
+
+# Saving the outcome to csv file.
+dff.to_csv('Somatic_0.4_Chr17.csv', sep='\t', index = None)
---
+++ Allele_Frequency/VarScan/Plots/Chr/VarScan/Somatic_0.4_Chr/Plot_AF.py
@@ -0,0 +1,24 @@
+# Importing the needed packages.
+import matplotlib.pyplot as plt
+import pandas as pd
+import csv
+
+# Mentioning the column names and inputing the csv file.
+column_names = ["POS", "ALLELE"]
+df = pd.read_csv("Somatic_0.4_Chr17.csv", sep= "\t", names=column_names)
+
+# Converting the column into a list.
+POS_List = df.POS.to_list()
+FREQ_List = df.ALLELE.to_list()
+
+# Printing the list.
+print(POS_List)
+print(FREQ_List)
+
+# Showing the output and saving it into a PDF.
+plt.plot(POS_List, FREQ_List, label='Postions vs Allele Frequency')
+plt.legend()
+plt.xticks([])
+plt.yticks([])
+plt.show()
+plt.savefig('Somatic_0.4_Chr17_Plot.pdf')
