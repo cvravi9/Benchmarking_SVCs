05a756aceffa0f2f35928295129e123c84d34de9
cvravi9 <cvravi9@gmail.com>
2021-02-17 16:28:31 +0100

    Adding a modified plot.

--- Allele_Frequency/Test/Scatter.py
+++ Allele_Frequency/Test/Scatter.py
@@ -13,14 +13,24 @@ df = pd.read_csv("Final_Miracum_AF_Values.csv", sep= "\t", names=column_names)
 # Converting the column into a list.
 CHROM_POS_List = df.CHROM_POS.to_list()
 Difference_List = df.VarScan_Value.to_list()
+Sum_List = df.Strelka_Value.to_list()
 
 print(CHROM_POS_List)
 print(Difference_List)
+print(Sum_List)
 
 # Final_Difference = Difference_List.astype(np.float)
 # [float(i) for i in Difference_List]
 
-plt.scatter(CHROM_POS_List, Difference_List, s=None, alpha=0.1)
+# Fixing random state for reproducibility
+np.random.seed(19680801)
+
+
+N = 27743
+colors = np.random.rand(N)
+
+plt.scatter(CHROM_POS_List, Difference_List, s=None, c=colors, alpha=0.1, label='Postions vs VarScan')
+plt.scatter(CHROM_POS_List, Sum_List, s=None, c=colors, alpha=0.1, label='Postions vs Strelka')
 # plt.xticks([])
 # plt.yticks([])
 plt.legend()
