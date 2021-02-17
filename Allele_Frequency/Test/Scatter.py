2f0412bdde841ecc559d75b01628d6185f1e5feb
cvravi9 <cvravi9@gmail.com>
2021-02-17 16:05:47 +0100

    Adding a new files.

--- Allele_Frequency/Test/Scatter.py
+++ Allele_Frequency/Test/Scatter.py
@@ -8,11 +8,11 @@ import csv
 column_names = ["CHROM_POS", "VarScan", "Strelka", "Equal", "VarScan_Chr", "VarScan_Value", "Strelka_Chr", "Strelka_Value", "Difference"]
 df = pd.read_csv("Final_Miracum_AF_Values.csv", sep= "\t", names=column_names)
 
-# df['Difference'] = df['Difference'].astype(float)
+# df['Difference'] = df['Difference'].astype(int)
         
 # Converting the column into a list.
 CHROM_POS_List = df.CHROM_POS.to_list()
-Difference_List = df.Difference.to_list()
+Difference_List = df.VarScan_Value.to_list()
 
 print(CHROM_POS_List)
 print(Difference_List)
@@ -20,9 +20,9 @@ print(Difference_List)
 # Final_Difference = Difference_List.astype(np.float)
 # [float(i) for i in Difference_List]
 
-plt.plot(CHROM_POS_List, Difference_List)
-plt.xticks([])
-plt.yticks([])
+plt.scatter(CHROM_POS_List, Difference_List, s=None, alpha=0.1)
+# plt.xticks([])
+# plt.yticks([])
 plt.legend()
 plt.show()
 plt.savefig('Test_Scatter.pdf')
