d3b664e7614249714ea86cd5e080398f7b158eae
cvravi9 <cvravi9@gmail.com>
2021-02-19 18:58:56 +0100

    Modified files.

--- ALT/Comparisions/Germline/Miracum_0.4/Merge.py
+++ ALT/Comparisions/Germline/Miracum_0.4/Merge.py
@@ -3,16 +3,17 @@ import numpy as np
 import pandas as pd
 
 # Reading csv files and concatinating "CHROM" and "POS"
-df = pd.read_csv("Miracum_0.4_Count.csv", sep = '\t', index_col= False)
-df1 = pd.read_csv("Miracum_0.7_Count.csv", sep = '\t', index_col= False)
-df2 = pd.read_csv("Somatic_0.4_Count.csv", sep = '\t', index_col= False)
-df3 = pd.read_csv("Somatic_0.7_Count.csv", sep = '\t', index_col= False)
+df = pd.read_csv("FreeBayes_Miracum_0.4.csv", sep = '\t', index_col= False)
+df1 = pd.read_csv("Strelka_Miracum_0.4.csv", sep = '\t', index_col= False)
+df2 = pd.read_csv("VarScan_Miracum_0.4.csv", sep = '\t', index_col= False)
 
 # Merging columns based on "CHROM-POS"
 First = pd.merge(df, df1, on=['ALT', 'REF'])
 Second = pd.merge(First, df2, on=['ALT', 'REF'])
-Third = pd.merge(Second, df3, on=['ALT', 'REF'])
-print(Third)
+
+# Mentioning the column names and inputing the csv file.
+Second.columns = ['REF', 'ALT', 'FreeBayes_Miracum_0.4', 'Strelka_Miracum_0.4', 'VarScan_Miracum_0.4']
+print(Second)
 
 # Saving the results in csv.
-Third.to_csv('All_Counts.csv', index=False, encoding='utf-8')
+Second.to_csv('Counts_Comparisions.csv', index=False, encoding='utf-8')
