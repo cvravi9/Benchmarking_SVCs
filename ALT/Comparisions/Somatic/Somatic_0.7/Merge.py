7eac41602a832e7dd2c2e4abf5de020020d2ebdb
cvravi9 <cvravi9@gmail.com>
2021-02-19 19:27:01 +0100

    Adding code and csv files.

--- ALT/Comparisions/Somatic/Somatic_0.7/Merge.py
+++ ALT/Comparisions/Somatic/Somatic_0.7/Merge.py
@@ -3,14 +3,14 @@ import numpy as np
 import pandas as pd
 
 # Reading csv files and concatinating "CHROM" and "POS"
-df = pd.read_csv("Strelka_Miracum_0.4.csv", sep = '\t', index_col= False)
-df1 = pd.read_csv("VarScan_Miracum_0.4.csv", sep = '\t', index_col= False)
+df = pd.read_csv("Strelka_Somatic_0.7.csv", sep = '\t', index_col= False)
+df1 = pd.read_csv("VarScan_Somatic_0.7.csv", sep = '\t', index_col= False)
 
 # Merging columns based on "CHROM-POS"
 First = pd.merge(df, df1, on=['ALT', 'REF'])
 
 # Mentioning the column names and inputing the csv file.
-First.columns = ['REF', 'ALT', 'Strelka_Miracum_0.4', 'VarScan_Miracum_0.4']
+First.columns = ['REF', 'ALT', 'Strelka_Somatic_0.7', 'VarScan_Somatic_0.7']
 print(First)
 
 # Saving the results in csv.
