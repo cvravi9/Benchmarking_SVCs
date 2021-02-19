ad445de0174342eab84d51c28a9399e0707901a6
cvravi9 <cvravi9@gmail.com>
2021-02-19 11:09:02 +0100

    Adding counts.

--- ALT/VarScan/Germline/Count.py
+++ ALT/VarScan/Germline/Count.py
@@ -10,65 +10,94 @@ dff.columns = ['CHROM_POS', 'REF', 'ALT']
 
 # Printing the list.
 dff1 = dff[(dff['REF'] == 'A') & (dff['ALT'] == 'A')]
+AA = len(dff1.index)
 print('The number of REF as A and ALT as A is')
-print(len(dff1.index))
+print(AA)
 
 dff2 = dff[(dff['REF'] == 'A') & (dff['ALT'] == 'T')]
+AT = len(dff2.index)
 print('The number of REF as A and ALT as T is')
-print(len(dff2.index))
+print(AT)
 
 dff3 = dff[(dff['REF'] == 'A') & (dff['ALT'] == 'G')]
+AG = len(dff3.index)
 print('The number of REF as A and ALT as G is')
-print(len(dff3.index))
+print(AG)
 
 dff4 = dff[(dff['REF'] == 'A') & (dff['ALT'] == 'C')]
+AC = len(dff4.index)
 print('The number of REF as A and ALT as C is')
-print(len(dff4.index))
+print(AC)
 
 dff5 = dff[(dff['REF'] == 'T') & (dff['ALT'] == 'T')]
+TT = len(dff5.index)
 print('The number of REF as T and ALT as T is')
-print(len(dff5.index))
+print(TT)
 
 dff6 = dff[(dff['REF'] == 'T') & (dff['ALT'] == 'A')]
+TA = len(dff6.index)
 print('The number of REF as T and ALT as A is')
-print(len(dff6.index))
+print(TA)
 
 dff7 = dff[(dff['REF'] == 'T') & (dff['ALT'] == 'G')]
+TG = len(dff7.index)
 print('The number of REF as T and ALT as G is')
-print(len(dff7.index))
+print(TG)
 
 dff8 = dff[(dff['REF'] == 'T') & (dff['ALT'] == 'C')]
+TC = len(dff8.index)
 print('The number of REF as T and ALT as C is')
-print(len(dff8.index))
+print(TC)
 
 dff9 = dff[(dff['REF'] == 'G') & (dff['ALT'] == 'G')]
+GG = len(dff9.index)
 print('The number of REF as G and ALT as G is')
-print(len(dff9.index))
+print(GG)
 
 dff10 = dff[(dff['REF'] == 'G') & (dff['ALT'] == 'A')]
+GA = len(dff10.index)
 print('The number of REF as G and ALT as A is')
-print(len(dff10.index))
+print(GA)
 
 dff11 = dff[(dff['REF'] == 'G') & (dff['ALT'] == 'T')]
+GT = len(dff11.index)
 print('The number of REF as G and ALT as T is')
-print(len(dff11.index))
+print(GT)
 
 dff12 = dff[(dff['REF'] == 'G') & (dff['ALT'] == 'C')]
+GC = len(dff12.index)
 print('The number of REF as G and ALT as C is')
-print(len(dff12.index))
+print(GC)
 
 dff13 = dff[(dff['REF'] == 'C') & (dff['ALT'] == 'C')]
+CC = len(dff13.index)
 print('The number of REF as C and ALT as C is')
-print(len(dff13.index))
+print(CC)
 
 dff14 = dff[(dff['REF'] == 'C') & (dff['ALT'] == 'A')]
+CA = len(dff14.index)
 print('The number of REF as C and ALT as A is')
-print(len(dff14.index))
+print(CA)
 
 dff15 = dff[(dff['REF'] == 'C') & (dff['ALT'] == 'T')]
+CT = len(dff15.index)
 print('The number of REF as C and ALT as T is')
-print(len(dff15.index))
+print(CT)
 
 dff16 = dff[(dff['REF'] == 'C') & (dff['ALT'] == 'G')]
+CG = len(dff16.index)
 print('The number of REF as C and ALT as G is')
-print(len(dff16.index))
+print(CG)
+
+# Delcaring a new dataframe.
+df = []
+
+# Taking all combinations as a list.
+data = {'REF': ['A', 'A', 'A', 'A', 'T', 'T', 'T', 'T', 'G', 'G', 'G', 'G', 'C', 'C', 'C', 'C'], 'ALT': ['A', 'T', 'G', 'C', 'T', 'A', 'G', 'C', 'G', 'A', 'T', 'C', 'C', 'A', 'T', 'G'], 'Miracum_0.4': [AA, AT, AG, AC, TT, TA, TG, TC, GG, GA, GT, GC, CC, CA, CT, CG]}
+
+# Collecting it into a dataframe.
+df = pd.DataFrame(data)
+print(df)
+
+# Exporting the outcome into CSV.
+df.to_csv('Miracum_0.4_Count.csv', index=False, encoding='utf-8')
