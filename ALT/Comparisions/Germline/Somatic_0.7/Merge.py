fcc5aa853f504d187451e3574bfd562b5e5b6a90
cvravi9 <cvravi9@gmail.com>
2021-02-19 19:10:55 +0100

    Adding code.

--- ALT/Comparisions/Germline/Somatic_0.7/Merge.py
+++ ALT/Comparisions/Germline/Somatic_0.7/Merge.py
@@ -3,16 +3,16 @@ import numpy as np
 import pandas as pd
 
 # Reading csv files and concatinating "CHROM" and "POS"
-df = pd.read_csv("FreeBayes_Miracum_0.4.csv", sep = '\t', index_col= False)
-df1 = pd.read_csv("Strelka_Miracum_0.4.csv", sep = '\t', index_col= False)
-df2 = pd.read_csv("VarScan_Miracum_0.4.csv", sep = '\t', index_col= False)
+df = pd.read_csv("FreeBayes_Somatic_0.7.csv", sep = '\t', index_col= False)
+df1 = pd.read_csv("Strelka_Somatic_0.7.csv", sep = '\t', index_col= False)
+df2 = pd.read_csv("VarScan_Somatic_0.7.csv", sep = '\t', index_col= False)
 
 # Merging columns based on "CHROM-POS"
 First = pd.merge(df, df1, on=['ALT', 'REF'])
 Second = pd.merge(First, df2, on=['ALT', 'REF'])
 
 # Mentioning the column names and inputing the csv file.
-Second.columns = ['REF', 'ALT', 'FreeBayes_Miracum_0.4', 'Strelka_Miracum_0.4', 'VarScan_Miracum_0.4']
+Second.columns = ['REF', 'ALT', 'FreeBayes_Somatic_0.7', 'Strelka_Somatic_0.7', 'VarScan_Somatic_0.7']
 print(Second)
 
 # Saving the results in csv.
