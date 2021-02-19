949db85e85a599352cf236ce369155601697dcea
cvravi9 <cvravi9@gmail.com>
2021-02-19 19:05:40 +0100

    Adding count comparisions.

---
+++ ALT/Comparisions/Germline/Somatic_0.7/Merge.py
@@ -0,0 +1,19 @@
+# Importing packages.
+import numpy as np
+import pandas as pd
+
+# Reading csv files and concatinating "CHROM" and "POS"
+df = pd.read_csv("FreeBayes_Miracum_0.4.csv", sep = '\t', index_col= False)
+df1 = pd.read_csv("Strelka_Miracum_0.4.csv", sep = '\t', index_col= False)
+df2 = pd.read_csv("VarScan_Miracum_0.4.csv", sep = '\t', index_col= False)
+
+# Merging columns based on "CHROM-POS"
+First = pd.merge(df, df1, on=['ALT', 'REF'])
+Second = pd.merge(First, df2, on=['ALT', 'REF'])
+
+# Mentioning the column names and inputing the csv file.
+Second.columns = ['REF', 'ALT', 'FreeBayes_Miracum_0.4', 'Strelka_Miracum_0.4', 'VarScan_Miracum_0.4']
+print(Second)
+
+# Saving the results in csv.
+Second.to_csv('Counts_Comparisions.csv', index=False, encoding='utf-8')
