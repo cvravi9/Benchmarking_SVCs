46438ce83136947e82d0107c8808a0afe4483fd8
cvravi9 <cvravi9@gmail.com>
2021-02-20 19:22:17 +0100

    Adding insertions and deletions.

--- Actions/Strelka/Somatic/Merge.py
+++ Actions/Strelka/Somatic/Merge.py
@@ -3,14 +3,12 @@ import numpy as np
 import pandas as pd
 
 # Reading csv files and concatinating "CHROM" and "POS"
-df = pd.read_csv("Insertions_Counts.csv", sep = '\t', index_col= False)
-df1 = pd.read_csv("Deletions_Counts.csv", sep = '\t', index_col= False)
-df2 = pd.read_csv("SNVs_Counts.csv", sep = '\t', index_col= False)
+df = pd.read_csv("Strelka_Somatic_Insertions_Counts.csv", sep = '\t', index_col= False)
+df1 = pd.read_csv("Strelka_Somatic_Deletions_Counts.csv", sep = '\t', index_col= False)
 
 # Merging columns based on "CHROM-POS"
 First = pd.merge(df, df1, on=['Type'])
-Second = pd.merge(First, df2, on=['Type'])
-print(Second)
+print(First)
 
 # Saving the results in csv.
-Second.to_csv('FreeBayes_Comparision.csv', index=False, encoding='utf-8')
+First.to_csv('Strelka_Somatic_Comparision.csv', index=False, encoding='utf-8')
