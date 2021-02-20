0d4acbdaaa9ac080d56d54407419d5c425eaecd7
cvravi9 <cvravi9@gmail.com>
2021-02-20 20:10:23 +0100

    Deleting files.

--- Actions/FreeBayes/Count.py
+++ Actions/FreeBayes/Count.py
@@ -6,11 +6,11 @@ import pandas as pd
 df = []
 
 # Taking all combinations as a list.
-data = {'Type': ['Miracum_0.4', 'Miracum_0.7', 'Somatic_0.4', 'Somatic_0.7'], 'SNVs_Count': ['1011639', '1011639', '1011640', '1011639']}
+data = {'Type': ['Miracum_0.4', 'Miracum_0.7', 'Somatic_0.4', 'Somatic_0.7'], 'FreeBayes_Insertions_Count': ['3181', '3181', '3179', '3181']}
 
 # Collecting it into a dataframe.
 df = pd.DataFrame(data)
 print(df)
 
 # Saving the results in csv.
-df.to_csv('SNVs_Counts.csv', sep='\t', index = None)
+df.to_csv('FreeBayes_Insertions_Counts.csv', sep='\t', index = None)
