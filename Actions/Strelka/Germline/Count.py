6dacd33894ab4e9376600c0bc91f75740d42bf53
cvravi9 <cvravi9@gmail.com>
2021-02-20 19:08:31 +0100

    Modified files.

--- Actions/Strelka/Germline/Count.py
+++ Actions/Strelka/Germline/Count.py
@@ -6,11 +6,11 @@ import pandas as pd
 df = []
 
 # Taking all combinations as a list.
-data = {'Type': ['Miracum_0.4', 'Miracum_0.7', 'Somatic_0.4', 'Somatic_0.7'], 'Stelka_Germline_SNVs_Count': ['97738', '97738', '97738', '97738']}
+data = {'Type': ['Miracum_0.4', 'Miracum_0.7', 'Somatic_0.4', 'Somatic_0.7'], 'Strelka_Germline_Deletions_Count': ['3527', '3527', '3527', '3527']}
 
 # Collecting it into a dataframe.
 df = pd.DataFrame(data)
 print(df)
 
 # Saving the results in csv.
-df.to_csv('Strelka_Germline_SNVs_Counts.csv', sep='\t', index = None)
+df.to_csv('Strelka_Germline_Deletions_Counts.csv', sep='\t', index = None)
