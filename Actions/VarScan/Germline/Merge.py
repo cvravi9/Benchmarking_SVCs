60eb0f80befd33d80256cc4c375c5e278da512af
cvravi9 <cvravi9@gmail.com>
2021-02-22 18:05:15 +0100

    Adding VarScan Germline Counts.

--- Actions/VarScan/Germline/Merge.py
+++ Actions/VarScan/Germline/Merge.py
@@ -3,9 +3,9 @@ import numpy as np
 import pandas as pd
 
 # Reading csv files and concatinating "CHROM" and "POS"
-df = pd.read_csv("Insertions_Counts.csv", sep = '\t', index_col= False)
-df1 = pd.read_csv("Deletions_Counts.csv", sep = '\t', index_col= False)
-df2 = pd.read_csv("SNVs_Counts.csv", sep = '\t', index_col= False)
+df = pd.read_csv("VarScan_Germline_Insertions_Counts.csv", sep = '\t', index_col= False)
+df1 = pd.read_csv("VarScan_Germline_Deletions_Counts.csv", sep = '\t', index_col= False)
+df2 = pd.read_csv("VarScan_Germline_SNVs_Counts.csv", sep = '\t', index_col= False)
 
 # Merging columns based on "CHROM-POS"
 First = pd.merge(df, df1, on=['Type'])
@@ -13,4 +13,4 @@ Second = pd.merge(First, df2, on=['Type'])
 print(Second)
 
 # Saving the results in csv.
-Second.to_csv('FreeBayes_Comparision.csv', index=False, encoding='utf-8')
+Second.to_csv('VarScan_Germline_Comparision.csv', index=False, encoding='utf-8')
