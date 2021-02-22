043e407e359d8e3ce41d1fd903c02563599e96cc
cvravi9 <cvravi9@gmail.com>
2021-02-22 17:37:59 +0100

    Adding REF and ALT Combinations.

---
+++ REF_ALT_Combinations/Comparisons/Somatic/Miracum_0.7/Background_Files/Two_Bar_Plot.py
@@ -0,0 +1,34 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import csv
+import numpy as np
+
+# Reading csv files and concatinating "CHROM" and "POS"
+dff = pd.read_csv("Indexed_Counts_Comparisions.csv", sep = '\t', index_col= False, error_bad_lines=False)
+dff.columns = ['REF', 'ALT', 'Strelka', 'VarScan']
+
+# set width of bar
+width = 0.25
+
+# Columns from the file
+a1 = dff.Strelka.to_list()
+a2 = dff.VarScan.to_list()
+
+# Set position of bar on X axis
+r1 = np.arange(len(a1))
+r2 = [x + width for x in r1]
+
+# Make the plot
+# plt.bar(r1, a1, color='#FFD700', width=width, edgecolor='white', label='FreeBayes_Miracum_0.4')
+plt.bar(r1, a1, color='#FFA500', width=width, edgecolor='white', label='Strelka_Miracum_0.7')
+plt.bar(r2, a2, color='#DC143C', width=width, edgecolor='white', label='VarScan_Miracum_0.7')
+
+# Add xticks on the middle of the group bars
+plt.xlabel('Combinations')
+plt.xticks([r + width for r in range(len(a1))], ['AA', 'AT', 'AG', 'AC', 'TT', 'TA', 'TG', 'TC', 'GG', 'GA', 'GT', 'GC', 'CC', 'CA', 'CT', 'CG'])
+
+# Create legend & Show graphic
+plt.legend()
+plt.show()
+plt.savefig('Two_Counts_Plot.pdf')
---
+++ REF_ALT_Combinations/VarScan/Germline/Count.py
@@ -0,0 +1,103 @@
+# Importing the needed packages.
+import numpy as np
+import pandas as pd
+
+# Importing the csv file.
+dff = pd.read_csv("Somatic_0.7.csv", sep = '\t', index_col= False)
+
+# Mentioning the column names and inputing the csv file.
+dff.columns = ['CHROM_POS', 'REF', 'ALT']
+
+# Printing the list.
+dff1 = dff[(dff['REF'] == 'A') & (dff['ALT'] == 'A')]
+AA = len(dff1.index)
+print('The number of REF as A and ALT as A is')
+print(AA)
+
+dff2 = dff[(dff['REF'] == 'A') & (dff['ALT'] == 'T')]
+AT = len(dff2.index)
+print('The number of REF as A and ALT as T is')
+print(AT)
+
+dff3 = dff[(dff['REF'] == 'A') & (dff['ALT'] == 'G')]
+AG = len(dff3.index)
+print('The number of REF as A and ALT as G is')
+print(AG)
+
+dff4 = dff[(dff['REF'] == 'A') & (dff['ALT'] == 'C')]
+AC = len(dff4.index)
+print('The number of REF as A and ALT as C is')
+print(AC)
+
+dff5 = dff[(dff['REF'] == 'T') & (dff['ALT'] == 'T')]
+TT = len(dff5.index)
+print('The number of REF as T and ALT as T is')
+print(TT)
+
+dff6 = dff[(dff['REF'] == 'T') & (dff['ALT'] == 'A')]
+TA = len(dff6.index)
+print('The number of REF as T and ALT as A is')
+print(TA)
+
+dff7 = dff[(dff['REF'] == 'T') & (dff['ALT'] == 'G')]
+TG = len(dff7.index)
+print('The number of REF as T and ALT as G is')
+print(TG)
+
+dff8 = dff[(dff['REF'] == 'T') & (dff['ALT'] == 'C')]
+TC = len(dff8.index)
+print('The number of REF as T and ALT as C is')
+print(TC)
+
+dff9 = dff[(dff['REF'] == 'G') & (dff['ALT'] == 'G')]
+GG = len(dff9.index)
+print('The number of REF as G and ALT as G is')
+print(GG)
+
+dff10 = dff[(dff['REF'] == 'G') & (dff['ALT'] == 'A')]
+GA = len(dff10.index)
+print('The number of REF as G and ALT as A is')
+print(GA)
+
+dff11 = dff[(dff['REF'] == 'G') & (dff['ALT'] == 'T')]
+GT = len(dff11.index)
+print('The number of REF as G and ALT as T is')
+print(GT)
+
+dff12 = dff[(dff['REF'] == 'G') & (dff['ALT'] == 'C')]
+GC = len(dff12.index)
+print('The number of REF as G and ALT as C is')
+print(GC)
+
+dff13 = dff[(dff['REF'] == 'C') & (dff['ALT'] == 'C')]
+CC = len(dff13.index)
+print('The number of REF as C and ALT as C is')
+print(CC)
+
+dff14 = dff[(dff['REF'] == 'C') & (dff['ALT'] == 'A')]
+CA = len(dff14.index)
+print('The number of REF as C and ALT as A is')
+print(CA)
+
+dff15 = dff[(dff['REF'] == 'C') & (dff['ALT'] == 'T')]
+CT = len(dff15.index)
+print('The number of REF as C and ALT as T is')
+print(CT)
+
+dff16 = dff[(dff['REF'] == 'C') & (dff['ALT'] == 'G')]
+CG = len(dff16.index)
+print('The number of REF as C and ALT as G is')
+print(CG)
+
+# Delcaring a new dataframe.
+df = []
+
+# Taking all combinations as a list.
+data = {'REF': ['A', 'A', 'A', 'A', 'T', 'T', 'T', 'T', 'G', 'G', 'G', 'G', 'C', 'C', 'C', 'C'], 'ALT': ['A', 'T', 'G', 'C', 'T', 'A', 'G', 'C', 'G', 'A', 'T', 'C', 'C', 'A', 'T', 'G'], 'Somatic_0.7': [AA, AT, AG, AC, TT, TA, TG, TC, GG, GA, GT, GC, CC, CA, CT, CG]}
+
+# Collecting it into a dataframe.
+df = pd.DataFrame(data)
+print(df)
+
+# Exporting the outcome into CSV.
+df.to_csv('Somatic_0.7_Count.csv', sep='\t', index = None)
