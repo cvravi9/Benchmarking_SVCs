c63bc41d17db52f4ef60e958d18990cea3d25563
cvravi9 <cvravi9@gmail.com>
2021-02-22 19:03:51 +0100

    Adding bar plot files.

---
+++ Actions/Comparisons/Somatic/Insertions/Background_Files/Merge.py
@@ -0,0 +1,15 @@
+# Importing packages.
+import numpy as np
+import pandas as pd
+
+# Reading csv files and concatinating "CHROM" and "POS"
+df = pd.read_csv("Strelka_Somatic_Insertions_Counts.csv", sep = '\t', index_col= False)
+df1 = pd.read_csv("VarScan_Somatic_Insertions_Counts.csv", sep = '\t', index_col= False)
+
+# Merging columns based on "CHROM-POS"
+First = pd.merge(df, df1, on=['Type'])
+print(First)
+
+# Saving the results in csv.
+First.to_csv('Somatic_Insertions_Comparision.csv', index=False, encoding='utf-8')
+First.to_csv('Indexed_Insertions_Comparisions.csv', sep='\t', index = None)
