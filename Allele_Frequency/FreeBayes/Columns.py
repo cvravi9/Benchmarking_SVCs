a89df81d5b2c506f3b88f0579e6ac5af467ac130
cvravi9 <cvravi9@gmail.com>
2021-02-24 18:11:04 +0100

    Adding Chr values.

--- Allele_Frequency/FreeBayes/Columns.py
+++ Allele_Frequency/FreeBayes/Columns.py
@@ -3,10 +3,10 @@ import numpy as np
 import pandas as pd
 
 # Reading csv files and concatinating "CHROM" and "POS"
-dff = pd.read_csv("FreeBayes_AF_Chr1_Values.csv", sep = ',', index_col= False)
+dff = pd.read_csv("FreeBayes_AF_ChrUn_Values.csv", sep = ',', index_col= False)
 print(dff)
 
 dff.columns = ['CHROM_POS', 'Miracum_0.4_AF', 'Miracum_0.7_AF',' Somatic_0.4_AF', 'Somatic_0.7_AF']
 
 # Saving the results in csv.
-dff.to_csv('FreeBayes_AF_Chr1_Values.csv', index=False, encoding='utf-8')
+dff.to_csv('FreeBayes_AF_ChrUn_Values.csv', index=False, encoding='utf-8')
