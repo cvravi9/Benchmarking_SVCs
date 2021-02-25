a5ce4d63717d02a8577195320136095d9090eaf6
cvravi9 <cvravi9@gmail.com>
2021-02-25 09:20:42 +0100

    Adding modified csv files.

--- Allele_Frequency/FreeBayes/Background_Files/Allele_Frequency.py
+++ Allele_Frequency/FreeBayes/Background_Files/Allele_Frequency.py
@@ -54,9 +54,9 @@ print(dff3)
 Result = pd.merge(dff, dff1, on="CHROM-POS")
 First_Result = pd.merge(Result, dff2, on="CHROM-POS")
 Second_Result = pd.merge(First_Result, dff3, on="CHROM-POS")
-Second_Result.columns = ['CHROM-POS', 'Miracum_0.4_AF', 'Miracum_0.7_AF', 'Somatic_0.4_AF', 'Somatic_0.7_AF']
+Second_Result.columns = ['CHROM_POS', 'Miracum_0.4_AF', 'Miracum_0.7_AF', 'Somatic_0.4_AF', 'Somatic_0.7_AF']
 print(Second_Result)
 
 # Saving the results in csv.
-Second_Result.to_csv('FreeBayes_AF_Values.csv', index=False, encoding='utf-8')
+Second_Result.to_csv('FreeBayes_AF_Values.csv', sep='\t', index=False, encoding='utf-8')
 Second_Result.to_csv('FreeBayes_AF_Plot_Values.csv', sep='\t', index =False)
