cf375818f29445783f857ad3363b94e27b013463
cvravi9 <cvravi9@gmail.com>
2021-02-25 10:42:29 +0100

    Adding allele frequency csv files.

--- Allele_Frequency/Comparisons/Somatic/Somatic_0.7/Merge.py
+++ Allele_Frequency/Comparisons/Somatic/Somatic_0.7/Merge.py
@@ -11,9 +11,9 @@ First = pd.merge(df, df1, on=['CHROM_POS'])
 
 # Assigning column names.
 First.columns = ['CHROM_POS', 'Strelka_Miracum_0.4', 'Strelka_Miracum_0.7', 'Strelka_Somatic_0.4', 'Strelka_Somatic_0.7', 'VarScan_Miracum_0.4', 'VarScan_Miracum_0.7', 'VarScan_Somatic_0.4', 'VarScan_Somatic_0.7']
-Second = First.drop(['CHROM_POS', 'Strelka_Miracum_0.7', 'Strelka_Somatic_0.4', 'Strelka_Somatic_0.7', 'VarScan_Miracum_0.7', 'VarScan_Somatic_0.4', 'VarScan_Somatic_0.7'], axis=1)
+Second = First.drop(['CHROM_POS', 'Strelka_Miracum_0.4', 'Strelka_Miracum_0.7', 'Strelka_Somatic_0.4', 'VarScan_Miracum_0.4', 'VarScan_Miracum_0.7', 'VarScan_Somatic_0.4'], axis=1)
 print(Second)
 
 # Saving the results in csv.
-Second.to_csv('All_Somatic_Miracum_0.4.csv', sep='\t', index=False, encoding='utf-8')
-Second.to_csv('All_Somatic_Miracum_0.4_Plot.csv', sep='\t', index = None)
+Second.to_csv('All_Somatic_Somatic_0.7.csv', sep='\t', index=False, encoding='utf-8')
+Second.to_csv('All_Somatic_Somatic_0.7_Plot.csv', sep='\t', index = None)
