a0383c441cc40b3c98101a01cf42b62b493b77bd
cvravi9 <cvravi9@gmail.com>
2021-03-10 17:11:41 +0100

    Modifying allele frequency coding.

--- Allele_Frequency/Strelka/SNVs/Setup.py
+++ Allele_Frequency/Strelka/SNVs/Setup.py
@@ -34,37 +34,47 @@ dff[['Tumor_DP', 'Tumor_FDP', 'Tumor_SDP', 'Tumor_SUBDP', 'Tumor_AU', 'Tumor_CU'
 dff = dff.drop(['FORMAT', 'NORMAL', 'TUMOR', 'Normal_DP', 'Normal_FDP', 'Normal_SDP', 'Normal_SUBDP', 'Tumor_DP', 'Tumor_FDP', 'Tumor_SDP', 'Tumor_SUBDP'], axis=1)
 
 for i in dff['CHROM_POS']:
-    dff.loc[dff['REF_U'] == 'AU', 'Normal'] = dff.Normal_AU
-    dff.loc[dff['REF_U'] == 'CU', 'Normal'] = dff.Normal_CU
-    dff.loc[dff['REF_U'] == 'GU', 'Normal'] = dff.Normal_GU
-    dff.loc[dff['REF_U'] == 'TU', 'Normal'] = dff.Normal_TU
-    dff.loc[dff['ALT_U'] == 'AU', 'Tumor'] = dff.Tumor_AU
-    dff.loc[dff['ALT_U'] == 'CU', 'Tumor'] = dff.Tumor_CU
-    dff.loc[dff['ALT_U'] == 'GU', 'Tumor'] = dff.Tumor_GU
-    dff.loc[dff['ALT_U'] == 'TU', 'Tumor'] = dff.Tumor_TU
+    dff.loc[dff['REF_U'] == 'AU', 'REF_Normal'] = dff.Normal_AU
+    dff.loc[dff['REF_U'] == 'CU', 'REF_Normal'] = dff.Normal_CU
+    dff.loc[dff['REF_U'] == 'GU', 'REF_Normal'] = dff.Normal_GU
+    dff.loc[dff['REF_U'] == 'TU', 'REF_Normal'] = dff.Normal_TU
+    dff.loc[dff['ALT_U'] == 'AU', 'ALT_Normal'] = dff.Normal_AU
+    dff.loc[dff['ALT_U'] == 'CU', 'ALT_Normal'] = dff.Normal_CU
+    dff.loc[dff['ALT_U'] == 'GU', 'ALT_Normal'] = dff.Normal_GU
+    dff.loc[dff['ALT_U'] == 'TU', 'ALT_Normal'] = dff.Normal_TU
+print(dff)
 
 # Creating new columns by splitting the "NORMAL" and "TUMOR" columns by ':' and renaming the new columns based on the format "DP:FDP:SDP:SUBDP:AU:CU:GU:TU"
-dff[['Normal_First', 'Normal_Second']] = dff['Normal'].str.split(',',expand=True)
-dff[['Tumor_First', 'Tumor_Second']] = dff['Tumor'].str.split(',',expand=True)
+dff[['REF_Normal_First', 'REF_Normal_Second']] = dff['REF_Normal'].str.split(',',expand=True)
+dff[['ALT_Normal_First', 'ALT_Normal_Second']] = dff['ALT_Normal'].str.split(',',expand=True)
+print(dff)
 
 # Dropping of the unnecessary columns and reorganising the columns.
-dff = dff.drop(['REF_U', 'ALT_U', 'Normal_AU', 'Normal_CU', 'Normal_GU', 'Normal_TU', 'Tumor_AU', 'Tumor_CU', 'Tumor_GU', 'Tumor_TU', 'Normal','Tumor', 'Normal_Second', 'Tumor_Second'], axis=1)
+dff = dff.drop(['REF_U', 'ALT_U', 'Normal_AU', 'Normal_CU', 'Normal_GU', 'Normal_TU', 'Tumor_AU', 'Tumor_CU', 'Tumor_GU', 'Tumor_TU', 'REF_Normal_Second', 'ALT_Normal_Second'], axis=1)
+print(dff)
 
 # Naming the columns after importing the csv file.
-dff.columns = ['CHROM_POS', 'REF', 'ALT', 'NORMAL', 'TUMOR']
+dff.columns = ['CHROM_POS', 'REF', 'ALT', 'REF_NORMAL', 'ALT_NORMAL', 'REF_Normal_Value', 'ALT_Normal_Value']
+print(dff)
 
-# Converting string values columans to int.
-dff['NORMAL'] = dff['NORMAL'].astype(int)
-dff['TUMOR'] = dff['TUMOR'].astype(int)
+# Converting string values columns to int.
+dff['REF_Normal_Value'] = dff['REF_Normal_Value'].astype(int)
+dff['ALT_Normal_Value'] = dff['ALT_Normal_Value'].astype(int)
 
 # Adding columns for single read depth value.
-dff['SUM'] = dff["NORMAL"] + dff["TUMOR"]
+dff['SUM'] = dff["REF_Normal_Value"] + dff["ALT_Normal_Value"]
 
 # Getting Allele Frequency
-dff['Allele_Frequency'] = dff['TUMOR']/dff['SUM']
+dff['Normal'] = dff['ALT_Normal_Value']/dff['SUM']
+
+# Converting string values columans to int.
+dff['Normal'] = dff['Normal'].astype(float).round(2)
+
+# Concatinating the "CHROM" and "POS"
+dff["Normal"] = dff['REF'].astype(str) + ':' + dff['Normal'].astype(str)
 
 # Dropping of the unnecessary columns and reorganising the columns.
-dff = dff.drop(['NORMAL', 'TUMOR', 'SUM'], axis=1)
+dff = dff.drop(['REF', 'ALT', 'REF_NORMAL', 'ALT_NORMAL', 'REF_Normal_Value', 'ALT_Normal_Value', 'SUM'], axis=1)
 print(dff)
 
 # Saving the result into a csv file for plotting.
