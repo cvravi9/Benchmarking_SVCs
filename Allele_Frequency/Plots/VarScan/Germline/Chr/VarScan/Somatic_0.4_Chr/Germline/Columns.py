02278f75943ecce71bef3ee034633831f729a31a
cvravi9 <cvravi9@gmail.com>
2021-03-11 21:28:12 +0100

    Adding modified plot files.

---
+++ Allele_Frequency/Plots/VarScan/Germline/Chr/VarScan/Somatic_0.4_Chr/Germline/Columns.py
@@ -0,0 +1,10 @@
+# Importing packages.
+import numpy as np
+import pandas as pd
+
+# Importing vcf files and assigning columns.
+dff = pd.read_csv("Miracum_0.4_Chr17_Germline.vcf", sep = '\t', index_col= False)
+dff.columns = ['#CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER', 'INFO', 'FORMAT', 'NORMAL', 'TUMOR']
+
+# Saving the outcome to csv file.
+dff.to_csv('Miracum_0.4_Chr17_Germline.vcf', sep='\t', index = None)
