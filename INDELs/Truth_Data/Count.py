9085f2e1ae58431eb7088d031b259288f6492c50
cvravi9 <cvravi9@gmail.com>
2021-03-12 22:17:43 +0100

    Adding modified csv files.

--- INDELs/Truth_Data/Count.py
+++ INDELs/Truth_Data/Count.py
@@ -6,11 +6,11 @@ import pandas as pd
 df = []
 
 # Taking all combinations as a list.
-data = {'Type': ['Somatic_Truth'], 'Insertions_Count': ['44592']}
+data = {'Type': ['Somatic_Truth'], 'Deletions_Count': ['53883']}
 
 # Collecting it into a dataframe.
 df = pd.DataFrame(data)
 print(df)
 
 # Saving the results in csv.
-df.to_csv('Somatic_Truth_Insertions_Counts.csv', sep='\t', index = None)
+df.to_csv('Somatic_Truth_Deletions_Counts.csv', sep=',', index = None)
