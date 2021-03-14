a13166dcb2470ab133a2b3ea88a01964978eed9f
cvravi9 <cvravi9@gmail.com>
2021-03-14 15:29:33 +0100

    Adding modified csv file.

--- SNVs_Positions/Comparisons/Tumor_Purity_0.3/Background_Files/Numbers.py
+++ SNVs_Positions/Comparisons/Tumor_Purity_0.3/Background_Files/Numbers.py
@@ -6,11 +6,11 @@ import pandas as pd
 df = []
 
 # Taking all combinations as a list.
-data = {'Type': ['Strelka_Positions', 'VarScan_Positions', 'Truth_Data_Positions', 'Strelka_and_VarScan_Positions', 'VarScan_and_Truth_Data_Positions', 'Truth_Data_and_Strelka_Positions', 'Strelka_VarScan_Truth_Data_Positions'], 'Positions': ['13315', '29316', '1104786', '3104', '2843', '2843', '1052']}
+data = {'Type': ['Strelka_Tumor_Purity_0.3', 'Strelka_Tumor_Purity_0.5', 'Strelka_Tumor_Purity_0.7'], 'Strelka_Positions': ['13315', '13315', '13315'], 'Strelka_SNPs': ['12897', '12897', '12897']}
 
 # Collecting it into a dataframe.
 df = pd.DataFrame(data)
 print(df)
 
 # Saving the results in csv.
-df.to_csv('Positions_Counts.csv', sep=',', index = None)
+df.to_csv('Strelka_Counts.csv', sep=',', index = None)
