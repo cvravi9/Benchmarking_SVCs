5a9971447cb3e0e8f277fd82ddec98b3e3bf62b3
cvravi9 <cvravi9@gmail.com>
2021-03-17 23:53:13 +0100

    Adding Somatic Truth Background Files.

---
+++ Allele_Frequency/Truth_Data/Background_Files/Somatic_Truth_AF.py
@@ -0,0 +1,19 @@
+# Importing packages.
+import numpy as np
+import pandas as pd
+
+# Reading csv files and concatinating "CHROM" and "POS"
+dff = pd.read_csv("Somatic_Truth.frq", sep = '\t', index_col= False, error_bad_lines=False)
+dff.columns = ['CHROM', 'POS', 'N_ALLELES', 'N_CHR', 'ALLELE:FREQ']
+dff["CHROM_POS"] = dff['CHROM'].astype(str) + '-' + dff['POS'].astype(str)
+
+# Reorganising columns.
+dff = dff.drop(['N_ALLELES', 'N_CHR', 'CHROM', 'POS'], axis=1)
+cols = dff.columns.tolist()
+cols = cols[-1:] + cols[:-1]
+dff = dff[cols]
+print(dff)
+
+# Saving the results in csv.
+dff.to_csv('Somatic_Truth.csv', sep='\t', index=False, encoding='utf-8')
+dff.to_csv('Somatic_Truth_Case.csv', sep='\t', index = None)
