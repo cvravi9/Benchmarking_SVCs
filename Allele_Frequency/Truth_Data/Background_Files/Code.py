2cb942dedcd352c590e39e54b07e5f8013bccf1d
cvravi9 <cvravi9@gmail.com>
2021-03-20 21:38:20 +0100

    Adding modified csv files.

---
+++ Allele_Frequency/Truth_Data/Background_Files/Code.py
@@ -0,0 +1,97 @@
+# Importing packages.
+import numpy as np
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+import csv
+
+# Reading csv files and concatinating "CHROM" and "POS"
+dff = pd.read_csv("Somatic_Truth.frq", sep = '\t', index_col= False, error_bad_lines=False)
+dff.columns = ['CHROM', 'POS', 'N_ALLELES', 'N_CHR', 'ALLELE:FREQ']
+dff["CHROM_POS"] = dff['CHROM'].astype(str) + '-' + dff['POS'].astype(str)
+
+# Reorganising columns.
+dff = dff.drop(['N_ALLELES', 'N_CHR', 'CHROM', 'POS'], axis=1)
+cols = dff.columns.tolist()
+cols = cols[-1:] + cols[:-1]
+dff = dff[cols]
+print(dff)
+
+# Saving the results in csv.
+dff.to_csv('Somatic_Truth.csv', sep=',', index = False)
+
+# Creating new columns by splitting the "NORMAL" and "TUMOR" columns by ':' and renaming the new columns based on the format "GT:GQ:DP:AD:ADF:ADR".
+dff[['Allele', 'Freq']] = dff['ALLELE:FREQ'].str.split(':',expand=True)
+print(dff)
+
+#  Dropping of the unnecessary columns and only choosing the "NORMAL Depth" i.e. "NORMAL-DP" and "TUMOR Depth" i.e. "TUMOR-DP"
+dff = dff.drop(['CHROM_POS', 'ALLELE:FREQ', 'Allele'], axis=1)
+print(dff)
+
+# Renaming the columns.
+dff.columns = ['Freq']
+print(dff)
+
+# Converting string values columns to float.
+dff['Freq'] = dff['Freq'].astype(float)
+print(dff)
+
+# Getting a count based on allele frequency values.
+dff1 = dff[dff < 0.26].count()
+dff2 = dff[dff < 0.51].count()
+dff3 = dff[dff < 0.76].count()
+dff4 = dff[dff < 1.01].count()
+
+# Getting the final values
+dff5 = (dff1 - dff2).abs()
+dff6 = (dff2 - dff3).abs()
+dff7 = (dff3 - dff4).abs()
+print(dff1)
+print(dff5)
+print(dff6)
+print(dff7)
+
+# Converting into list.
+First_Column = dff1.tolist()
+Second_Column = dff5.tolist()
+Third_Column = dff6.tolist()
+Fourth_Column = dff7.tolist()
+Fifth_Column =['Truth_Data']
+print(First_Column)
+print(Second_Column)
+print(Third_Column)
+print(Fourth_Column)
+
+# Declaring new columns.
+dff8 = pd.DataFrame(Fifth_Column, columns = ['Type'])
+dff8['Less than 0.25'] = First_Column
+dff8['Between 0.25 & 0.50'] = Second_Column
+dff8['Between 0.50 & 0.75'] = Third_Column
+dff8['Between 0.75 & 1.00'] = Fourth_Column
+print(dff8)
+
+# Saving the results in csv.
+dff8.to_csv('Truth_Data_AF_Counts.csv', sep=',', index = None)
+
+# Adding labels
+labels = ['<= 0.25', '<= 0.50', '<= 0.75', '<= 1.00']
+
+x = np.arange(len(labels))  # the label locations
+width = 0.35  # the width of the bars
+dff9 = dff8.drop(['Type'], axis=1)
+print(dff9)
+
+# Converting the values to a list
+List = dff9.values.tolist()
+print(List)
+
+fig, ax = plt.subplots()
+ax.bar(labels, List, width, label='Men')
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_title('Truth_Data_Value_Counts')
+
+# Create legend & Show graphic
+plt.legend()
+plt.show()
+plt.savefig('Truth_Data_AF_Plot.pdf')
