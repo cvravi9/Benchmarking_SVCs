613664f9933f136693dc17cb2bf1fdb0532eac91
cvravi9 <cvravi9@gmail.com>
2021-03-21 16:34:28 +0100

    Adding modified csv and code files.

---
+++ Allele_Frequency/VarScan/Background_Files/Code.py
@@ -0,0 +1,134 @@
+# Importing packages.
+import numpy as np
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+import csv
+
+# Reading csv files and concatinating "CHROM" and "POS"
+dff = pd.read_csv("VarScan_0.3.frq", sep = '\t', index_col= False)
+dff.columns = ['CHROM', 'POS', 'N_ALLELES', 'N_CHR', 'ALLELE:FREQ']
+dff["CHROM_POS"] = dff['CHROM'].astype(str) + '-' + dff['POS'].astype(str)
+
+dff2 = pd.read_csv("VarScan_0.5.frq", sep = '\t', index_col= False)
+dff2.columns = ['CHROM', 'POS', 'N_ALLELES', 'N_CHR', 'ALLELE:FREQ']
+dff2["CHROM_POS"] = dff2['CHROM'].astype(str) + '-' + dff2['POS'].astype(str)
+
+dff3 = pd.read_csv("VarScan_0.7.frq", sep = '\t', index_col= False)
+dff3.columns = ['CHROM', 'POS', 'N_ALLELES', 'N_CHR', 'ALLELE:FREQ']
+dff3["CHROM_POS"] = dff3['CHROM'].astype(str) + '-' + dff3['POS'].astype(str)
+
+# Reorganising columns.
+dff = dff.drop(['N_ALLELES', 'N_CHR', 'CHROM', 'POS'], axis=1)
+cols = dff.columns.tolist()
+cols = cols[-1:] + cols[:-1]
+dff = dff[cols]
+print(dff)
+
+dff2 = dff2.drop(['N_ALLELES', 'N_CHR', 'CHROM', 'POS'], axis=1)
+cols = dff2.columns.tolist()
+cols = cols[-1:] + cols[:-1]
+dff2 = dff2[cols]
+print(dff2)
+
+dff3 = dff3.drop(['N_ALLELES', 'N_CHR', 'CHROM', 'POS'], axis=1)
+cols = dff3.columns.tolist()
+cols = cols[-1:] + cols[:-1]
+dff3 = dff3[cols]
+print(dff3)
+
+# Merging columns based on "CHROM_POS"
+Result = pd.merge(dff, dff2, on="CHROM_POS")
+Merge = pd.merge(Result, dff3, on="CHROM_POS")
+Merge.columns = ['CHROM_POS', 'VarScan_0.3_AF', 'VarScan_0.5_AF', 'VarScan_0.7_AF']
+
+# Saving the results in csv.
+Merge.to_csv('VarScan_Allele_Frequencies.csv', sep=',', index = False)
+
+# Creating new columns by splitting the "Allele" and "Value" by ':'.
+Merge[['VarScan_0.3_Allele', 'VarScan_0.3_Value']] = Merge['VarScan_0.3_AF'].str.split(':',expand=True)
+Merge[['VarScan_0.5_Allele', 'VarScan_0.5_Value']] = Merge['VarScan_0.5_AF'].str.split(':',expand=True)
+Merge[['VarScan_0.7_Allele', 'VarScan_0.7_Value']] = Merge['VarScan_0.7_AF'].str.split(':',expand=True)
+print(Merge)
+
+# Dropping of the unnecessary columns and only choosing the "NORMAL Depth" i.e. "NORMAL-DP" and "TUMOR Depth" i.e. "TUMOR-DP"
+dff4 = Merge.drop(['CHROM_POS', 'VarScan_0.3_AF', 'VarScan_0.5_AF', 'VarScan_0.7_AF', 'VarScan_0.3_Allele', 'VarScan_0.5_Allele', 'VarScan_0.7_Allele'], axis=1)
+
+# Renaming the columns.
+dff4.columns = ['VarScan_0.3', 'VarScan_0.5', 'VarScan_0.7']
+print(dff)
+
+# Converting string values columns to float.
+dff4['VarScan_0.3'] = dff4['VarScan_0.3'].astype(float)
+dff4['VarScan_0.5'] = dff4['VarScan_0.5'].astype(float)
+dff4['VarScan_0.7'] = dff4['VarScan_0.7'].astype(float)
+print(dff4)
+
+# Getting a count based on allele frequency values.
+dff5 = dff4[dff4 < 0.26].count()
+dff6 = dff4[dff4 < 0.51].count()
+dff7 = dff4[dff4 < 0.76].count()
+dff8 = dff4[dff4 < 1.01].count()
+
+# Getting the final values
+dff9 = (dff5 - dff6).abs()
+dff10 = (dff6 - dff7).abs()
+dff11 = (dff7 - dff8).abs()
+print(dff5)
+print(dff9)
+print(dff10)
+print(dff11)
+
+# Converting into list.
+First_Column = dff5.tolist()
+Second_Column = dff9.tolist()
+Third_Column = dff10.tolist()
+Fourth_Column = dff11.tolist()
+Fifth_Column =['VarScan_0.3', 'VarScan_0.5', 'VarScan_0.7']
+print(First_Column)
+print(Second_Column)
+print(Third_Column)
+print(Fourth_Column)
+
+# Declaring new columns.
+dff8 = pd.DataFrame(Fifth_Column, columns = ['Type'])
+dff8['Less than 0.25'] = First_Column
+dff8['Between 0.25 & 0.50'] = Second_Column
+dff8['Between 0.50 & 0.75'] = Third_Column
+dff8['Between 0.75 & 1.00'] = Fourth_Column
+print(dff8)
+
+# Saving the results in csv.
+dff8.to_csv('VarScan_Allele_Frequency_Counts.csv', sep=',', index = None)
+
+dff9 = dff8.drop(['Type'], axis=1)
+print(dff9)
+
+# Converting the values to a list
+List = dff9.values.tolist()
+a1, a2, a3 = List
+print(a1)
+print(a2)
+print(a3)
+
+# set width of bar
+width = 0.15
+
+# Set position of bar on X axis
+r1 = np.arange(len(a1))
+r2 = [x + width for x in r1]
+r3 = [x + width for x in r2]
+
+# Make the plot
+plt.bar(r1, a1, color='#ff0000', width=width, edgecolor='white', label='VarScan_0.3')
+plt.bar(r2, a2, color='#ffa07a', width=width, edgecolor='white', label='VarScan_0.5')
+plt.bar(r3, a3, color='#f08080', width=width, edgecolor='white', label='VarScan_0.7')
+
+# Add xticks on the middle of the group bars
+plt.xlabel('VarScan_Allele_Frequencies')
+plt.xticks([r + width for r in range(len(a1))], ['<= 0.25', '<= 0.50', '<= 0.75', '<= 1.00'])
+
+# Create legend & Show graphic
+plt.legend()
+plt.show()
+plt.savefig('VarScan_Allele_Frequency_Plot.pdf')
