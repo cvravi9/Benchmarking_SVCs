7b28bcfc0d379f42b9f5cb6a3a4cbc2ff5807162
cvravi9 <cvravi9@gmail.com>
2021-03-21 20:18:49 +0100

    Adding modified csv and code files.

---
+++ Read_Depth/VarScan/Background_Files/Code.py
@@ -0,0 +1,137 @@
+# Importing the needed packages.
+import numpy as np
+import pandas as pd
+
+# Reading the csv input file that is obtained after performing the following operations on the vcf file.
+# Step 1 - 'cut -f 1-2,9-11 Input-VCF-File > Output-VCF-File'
+# Step 2 - 'sed '/^#/d' Output-VCF-File > Updated_Output-VCF-File'
+
+# The first step is to selected the needed columns in the vcf file.
+# The second step if to eliminate all lines that start with a '#'
+dff = pd.read_csv("Updated_VarScan_0.3.vcf", sep = '\t', index_col= False)
+dff1 = pd.read_csv("Updated_VarScan_0.5.vcf", sep = '\t', index_col= False)
+dff2 = pd.read_csv("Updated_VarScan_0.7.vcf", sep = '\t', index_col= False)
+
+# Naming the columns after importing the csv file.
+dff.columns = ['CHROM', 'POS', 'FORMAT', 'NORMAL', 'TUMOR']
+dff1.columns = ['CHROM', 'POS', 'FORMAT', 'NORMAL', 'TUMOR']
+dff2.columns = ['CHROM', 'POS', 'FORMAT', 'NORMAL', 'TUMOR']
+
+# Concatinating the "CHROM" and "POS"
+dff["CHROM_POS"] = dff['CHROM'].astype(str) + '-' + dff['POS'].astype(str)
+dff1["CHROM_POS"] = dff1['CHROM'].astype(str) + '-' + dff1['POS'].astype(str)
+dff2["CHROM_POS"] = dff2['CHROM'].astype(str) + '-' + dff2['POS'].astype(str)
+
+# Dropping of the unnecessary columns and reorganising the columns.
+dff = dff.drop(['CHROM', 'POS', 'FORMAT'], axis=1)
+cols = dff.columns.tolist()
+cols = cols[-1:] + cols[:-1]
+dff = dff[cols]
+print(dff)
+
+dff1 = dff1.drop(['CHROM', 'POS', 'FORMAT'], axis=1)
+cols = dff1.columns.tolist()
+cols = cols[-1:] + cols[:-1]
+dff1 = dff1[cols]
+print(dff1)
+
+dff2 = dff2.drop(['CHROM', 'POS', 'FORMAT'], axis=1)
+cols = dff2.columns.tolist()
+cols = cols[-1:] + cols[:-1]
+dff2 = dff2[cols]
+print(dff2)
+
+# Creating new columns by splitting the "NORMAL" and "TUMOR" columns by ':' and renaming the new columns based on the format "GT:GQ:DP:AD:ADF:ADR".
+dff[['NORMAL-GT','NORMAL-GQ','Normal_Read_Depth', 'NORMAL-AD','NORMAL-ADF', 'NORMAL-ADR']] = dff['NORMAL'].str.split(':',expand=True)
+dff[['TUMOR-GT','TUMOR-GQ','Tumor_Read_Depth', 'TUMOR-AD','TUMOR-ADF', 'TUMOR-ADR']] = dff['TUMOR'].str.split(':',expand=True)
+
+dff1[['NORMAL-GT','NORMAL-GQ','Normal_Read_Depth', 'NORMAL-AD','NORMAL-ADF', 'NORMAL-ADR']] = dff1['NORMAL'].str.split(':',expand=True)
+dff1[['TUMOR-GT','TUMOR-GQ','Tumor_Read_Depth', 'TUMOR-AD','TUMOR-ADF', 'TUMOR-ADR']] = dff1['TUMOR'].str.split(':',expand=True)
+
+dff2[['NORMAL-GT','NORMAL-GQ','Normal_Read_Depth', 'NORMAL-AD','NORMAL-ADF', 'NORMAL-ADR']] = dff2['NORMAL'].str.split(':',expand=True)
+dff2[['TUMOR-GT','TUMOR-GQ','Tumor_Read_Depth', 'TUMOR-AD','TUMOR-ADF', 'TUMOR-ADR']] = dff2['TUMOR'].str.split(':',expand=True)
+
+# Dropping of the unnecessary columns and only choosing the "NORMAL Depth" i.e. "NORMAL-DP" and "TUMOR Depth" i.e. "TUMOR-DP"
+dff = dff.drop(['NORMAL', 'TUMOR', 'NORMAL-GT', 'NORMAL-GQ', 'NORMAL-AD', 'NORMAL-ADF', 'NORMAL-ADR', 'TUMOR-GT', 'TUMOR-GQ', 'TUMOR-AD', 'TUMOR-ADF', 'TUMOR-ADR'], axis=1)
+print(dff)
+
+dff1 = dff1.drop(['NORMAL', 'TUMOR', 'NORMAL-GT', 'NORMAL-GQ', 'NORMAL-AD', 'NORMAL-ADF', 'NORMAL-ADR', 'TUMOR-GT', 'TUMOR-GQ', 'TUMOR-AD', 'TUMOR-ADF', 'TUMOR-ADR'], axis=1)
+print(dff1)
+
+dff2 = dff2.drop(['NORMAL', 'TUMOR', 'NORMAL-GT', 'NORMAL-GQ', 'NORMAL-AD', 'NORMAL-ADF', 'NORMAL-ADR', 'TUMOR-GT', 'TUMOR-GQ', 'TUMOR-AD', 'TUMOR-ADF', 'TUMOR-ADR'], axis=1)
+print(dff2)
+
+# Merging columns based on "CHROM-POS"
+First = pd.merge(dff, dff1, on=['CHROM_POS'])
+Second = pd.merge(First, dff2, on=['CHROM_POS'])
+
+# Renaming Columns
+Second.columns = ['CHROM_POS', 'VarScan_Normal_0.3', 'VarScan_Tumor_0.3', 'VarScan_Normal_0.5', 'VarScan_Tumor_0.5', 'VarScan_Normal_0.7', 'VarScan_Tumor_0.7']
+print(Second)
+
+# Saving the results in csv.
+Second.to_csv('VarScan_Read_Depth_Counts.csv', sep=',', index = None)
+
+# Finding the minimum values
+min1 = Second['VarScan_Normal_0.3'].min()
+min2 = Second['VarScan_Tumor_0.3'].min()
+min3 = Second['VarScan_Normal_0.5'].min()
+min4 = Second['VarScan_Tumor_0.5'].min()
+min5 = Second['VarScan_Normal_0.7'].min()
+min6 = Second['VarScan_Tumor_0.7'].min()
+
+# Finding the maximum values
+max1 = Second['VarScan_Normal_0.3'].max()
+max2 = Second['VarScan_Tumor_0.3'].max()
+max3 = Second['VarScan_Normal_0.5'].max()
+max4 = Second['VarScan_Tumor_0.5'].max()
+max5 = Second['VarScan_Normal_0.7'].max()
+max6 = Second['VarScan_Tumor_0.7'].max()
+
+# Finding the mean values
+mean1 = Second['VarScan_Normal_0.3'].mean()
+mean2 = Second['VarScan_Tumor_0.3'].mean()
+mean3 = Second['VarScan_Normal_0.5'].mean()
+mean4 = Second['VarScan_Tumor_0.5'].mean()
+mean5 = Second['VarScan_Normal_0.7'].mean()
+mean6 = Second['VarScan_Tumor_0.7'].mean()
+
+# Finding the minimum values
+median1 = Second['VarScan_Normal_0.3'].median()
+median2 = Second['VarScan_Tumor_0.3'].median()
+median3 = Second['VarScan_Normal_0.5'].median()
+median4 = Second['VarScan_Tumor_0.5'].median()
+median5 = Second['VarScan_Normal_0.7'].median()
+median6 = Second['VarScan_Tumor_0.7'].median()
+
+# Finding the minimum values
+mode1 = Second['VarScan_Normal_0.3'].mode()
+mode2 = Second['VarScan_Tumor_0.3'].mode()
+mode3 = Second['VarScan_Normal_0.5'].mode()
+mode4 = Second['VarScan_Tumor_0.5'].mode()
+mode5 = Second['VarScan_Normal_0.7'].mode()
+mode6 = Second['VarScan_Tumor_0.7'].mode()
+
+# Delcaring a new dataframe.
+df = pd.DataFrame()
+
+# Taking all combinations as a list.
+Type = ['VarScan_Normal_0.3', 'VarScan_Tumor_0.3', 'VarScan_Normal_0.5', 'VarScan_Tumor_0.5', 'VarScan_Normal_0.7', 'VarScan_Tumor_0.7']
+Minimum_Value = [min1, min2, min3, min4, min5, min6]
+Maximum_Value = [max1, max2, max3, max4, max5, max6]
+Mean_Value = [mean1, mean2, mean3, mean4, mean5, mean6]
+Median_Value = [median1, median2, median3, median4, median5, median6]
+Mode_Value = [mode1, mode2, mode3, mode4, mode5, mode6]
+
+# Adding columns
+df['Type'] = Type
+df['Minimum_Value'] = Minimum_Value
+df['Maximum_Value'] = Maximum_Value
+df['Mean_Value'] = Mean_Value
+df['Median_Value'] = Median_Value
+df['Mode_Value'] = Mode_Value
+# Collecting it into a dataframe.
+print(df)
+
+# Saving the results in csv.
+df.to_csv('VarScan_Read_Depth_Statistics.csv', sep=',', index = False)
