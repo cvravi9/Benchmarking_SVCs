a8c3fbd5d1fa02fa3eb2b5d500ed6c74465c375d
cvravi9 <cvravi9@gmail.com>
2021-03-23 09:28:30 +0100

    Adding modified csv and code files.

---
+++ Variants/Strelka/Background_Files/Code.py
@@ -0,0 +1,226 @@
+# Importing the needed packages.
+import numpy as np
+import pandas as pd
+import matplotlib
+from matplotlib import rc
+matplotlib.rcParams['mathtext.fontset'] = 'cm'
+matplotlib.rcParams['font.family'] = 'serif'
+import matplotlib.pyplot as plt
+import pandas as pd
+import csv
+import numpy as np
+
+# Reading the csv input file that is obtained after performing the following operations on the vcf file.
+# Step 1 - 'cut -f 1-2,4-5 Input.vcf > Output.vcf'
+# Step 2 - 'sed '/^#/d' Output.vcf > Updated.vcf'
+
+# The first step is to selected the needed columns in the vcf file.
+# The second step if to eliminate all lines that start with a '#'
+dff = pd.read_csv("Updated_Strelka_0.3_SNV.vcf", sep = '\t', index_col= False)
+dff1 = pd.read_csv("Updated_Strelka_0.5_SNV.vcf", sep = '\t', index_col= False)
+dff2 = pd.read_csv("Updated_Strelka_0.7_SNV.vcf", sep = '\t', index_col= False)
+
+# Naming the columns after importing the csv file.
+dff.columns = ['CHROM', 'POS', 'REF', 'ALT']
+dff1.columns = ['CHROM', 'POS', 'REF', 'ALT']
+dff2.columns = ['CHROM', 'POS', 'REF', 'ALT']
+
+# Concatinating the "CHROM" and "POS"
+dff["CHROM_POS"] = dff['CHROM'].astype(str) + '-' + dff['POS'].astype(str)
+dff1["CHROM_POS"] = dff1['CHROM'].astype(str) + '-' + dff1['POS'].astype(str)
+dff2["CHROM_POS"] = dff2['CHROM'].astype(str) + '-' + dff2['POS'].astype(str)
+
+# Dropping of the unnecessary columns and reorganising the columns.
+dff = dff.drop(['CHROM', 'POS'], axis=1)
+cols = dff.columns.tolist()
+cols = cols[-1:] + cols[:-1]
+dff = dff[cols]
+print(dff)
+
+dff1 = dff1.drop(['CHROM', 'POS'], axis=1)
+cols = dff1.columns.tolist()
+cols = cols[-1:] + cols[:-1]
+dff1 = dff1[cols]
+print(dff1)
+
+dff2 = dff2.drop(['CHROM', 'POS'], axis=1)
+cols = dff2.columns.tolist()
+cols = cols[-1:] + cols[:-1]
+dff2 = dff2[cols]
+print(dff2)
+
+# Mentioning the column names and inputing the csv file.
+dff.columns = ['CHROM_POS', 'REF', 'ALT']
+dff1.columns = ['CHROM_POS', 'REF', 'ALT']
+dff2.columns = ['CHROM_POS', 'REF', 'ALT']
+
+# Printing the list.
+dff4 = dff[(dff['REF'] == 'A') & (dff['ALT'] == 'A')]
+dff5 = dff1[(dff1['REF'] == 'A') & (dff1['ALT'] == 'A')]
+dff6 = dff2[(dff2['REF'] == 'A') & (dff2['ALT'] == 'A')]
+AA1 = len(dff4.index)
+AA2 = len(dff5.index)
+AA3 = len(dff6.index)
+
+dff7 = dff[(dff['REF'] == 'A') & (dff['ALT'] == 'T')]
+dff8 = dff1[(dff1['REF'] == 'A') & (dff1['ALT'] == 'T')]
+dff9 = dff2[(dff2['REF'] == 'A') & (dff2['ALT'] == 'T')]
+AT1 = len(dff7.index)
+AT2 = len(dff8.index)
+AT3 = len(dff9.index)
+
+dff10 = dff[(dff['REF'] == 'A') & (dff['ALT'] == 'G')]
+dff11 = dff1[(dff1['REF'] == 'A') & (dff1['ALT'] == 'G')]
+dff12 = dff2[(dff2['REF'] == 'A') & (dff2['ALT'] == 'G')]
+AG1 = len(dff10.index)
+AG2 = len(dff11.index)
+AG3 = len(dff12.index)
+
+dff13 = dff[(dff['REF'] == 'A') & (dff['ALT'] == 'C')]
+dff14 = dff1[(dff1['REF'] == 'A') & (dff1['ALT'] == 'C')]
+dff15 = dff2[(dff2['REF'] == 'A') & (dff2['ALT'] == 'C')]
+AC1 = len(dff13.index)
+AC2 = len(dff14.index)
+AC3 = len(dff15.index)
+
+dff16 = dff[(dff['REF'] == 'T') & (dff['ALT'] == 'T')]
+dff17 = dff1[(dff1['REF'] == 'T') & (dff1['ALT'] == 'T')]
+dff18 = dff2[(dff2['REF'] == 'T') & (dff2['ALT'] == 'T')]
+TT1 = len(dff16.index)
+TT2 = len(dff17.index)
+TT3 = len(dff18.index)
+
+dff19 = dff[(dff['REF'] == 'T') & (dff['ALT'] == 'A')]
+dff20 = dff1[(dff1['REF'] == 'T') & (dff1['ALT'] == 'A')]
+dff21 = dff2[(dff2['REF'] == 'T') & (dff2['ALT'] == 'A')]
+TA1 = len(dff19.index)
+TA2 = len(dff20.index)
+TA3 = len(dff21.index)
+
+dff22 = dff[(dff['REF'] == 'T') & (dff['ALT'] == 'G')]
+dff23 = dff1[(dff1['REF'] == 'T') & (dff1['ALT'] == 'G')]
+dff24 = dff2[(dff2['REF'] == 'T') & (dff2['ALT'] == 'G')]
+TG1 = len(dff22.index)
+TG2 = len(dff23.index)
+TG3 = len(dff24.index)
+
+dff25 = dff[(dff['REF'] == 'T') & (dff['ALT'] == 'C')]
+dff26 = dff1[(dff1['REF'] == 'T') & (dff1['ALT'] == 'C')]
+dff27 = dff2[(dff2['REF'] == 'T') & (dff2['ALT'] == 'C')]
+TC1 = len(dff25.index)
+TC2 = len(dff26.index)
+TC3 = len(dff27.index)
+
+dff28 = dff[(dff['REF'] == 'G') & (dff['ALT'] == 'G')]
+dff29 = dff1[(dff1['REF'] == 'G') & (dff1['ALT'] == 'G')]
+dff30 = dff2[(dff2['REF'] == 'G') & (dff2['ALT'] == 'G')]
+GG1 = len(dff28.index)
+GG2 = len(dff29.index)
+GG3 = len(dff30.index)
+
+dff31 = dff[(dff['REF'] == 'G') & (dff['ALT'] == 'A')]
+dff32 = dff1[(dff1['REF'] == 'G') & (dff1['ALT'] == 'A')]
+dff33 = dff2[(dff2['REF'] == 'G') & (dff2['ALT'] == 'A')]
+GA1 = len(dff31.index)
+GA2 = len(dff32.index)
+GA3 = len(dff33.index)
+
+dff34 = dff[(dff['REF'] == 'G') & (dff['ALT'] == 'T')]
+dff35 = dff1[(dff1['REF'] == 'G') & (dff1['ALT'] == 'T')]
+dff36 = dff2[(dff2['REF'] == 'G') & (dff2['ALT'] == 'T')]
+GT1 = len(dff34.index)
+GT2 = len(dff35.index)
+GT3 = len(dff36.index)
+
+dff37 = dff[(dff['REF'] == 'G') & (dff['ALT'] == 'C')]
+dff38 = dff1[(dff1['REF'] == 'G') & (dff1['ALT'] == 'C')]
+dff39 = dff2[(dff2['REF'] == 'G') & (dff2['ALT'] == 'C')]
+GC1 = len(dff37.index)
+GC2 = len(dff38.index)
+GC3 = len(dff39.index)
+
+dff40 = dff[(dff['REF'] == 'C') & (dff['ALT'] == 'C')]
+dff41 = dff1[(dff1['REF'] == 'C') & (dff1['ALT'] == 'C')]
+dff42 = dff2[(dff2['REF'] == 'C') & (dff2['ALT'] == 'C')]
+CC1 = len(dff40.index)
+CC2 = len(dff41.index)
+CC3 = len(dff42.index)
+
+dff43 = dff[(dff['REF'] == 'C') & (dff['ALT'] == 'A')]
+dff44 = dff1[(dff1['REF'] == 'C') & (dff1['ALT'] == 'A')]
+dff45 = dff2[(dff2['REF'] == 'C') & (dff2['ALT'] == 'A')]
+CA1 = len(dff43.index)
+CA2 = len(dff44.index)
+CA3 = len(dff45.index)
+
+dff46 = dff[(dff['REF'] == 'C') & (dff['ALT'] == 'T')]
+dff47 = dff1[(dff1['REF'] == 'C') & (dff1['ALT'] == 'T')]
+dff48 = dff2[(dff2['REF'] == 'C') & (dff2['ALT'] == 'T')]
+CT1 = len(dff46.index)
+CT2 = len(dff47.index)
+CT3 = len(dff48.index)
+
+dff49 = dff[(dff['REF'] == 'C') & (dff['ALT'] == 'G')]
+dff50 = dff1[(dff1['REF'] == 'C') & (dff1['ALT'] == 'G')]
+dff51 = dff2[(dff2['REF'] == 'C') & (dff2['ALT'] == 'G')]
+CG1 = len(dff49.index)
+CG2 = len(dff50.index)
+CG3 = len(dff51.index)
+
+# Delcaring a new dataframe.
+df = []
+df1 = []
+df2 = []
+
+# Taking all combinations as a list.
+data = {'REF': ['A', 'A', 'A', 'A', 'T', 'T', 'T', 'T', 'G', 'G', 'G', 'G', 'C', 'C', 'C', 'C'], 'ALT': ['A', 'T', 'G', 'C', 'T', 'A', 'G', 'C', 'G', 'A', 'T', 'C', 'C', 'A', 'T', 'G'], 'Strelka_0.3': [AA1, AT1, AG1, AC1, TT1, TA1, TG1, TC1, GG1, GA1, GT1, GC1, CC1, CA1, CT1, CG1]}
+data1 = {'REF': ['A', 'A', 'A', 'A', 'T', 'T', 'T', 'T', 'G', 'G', 'G', 'G', 'C', 'C', 'C', 'C'], 'ALT': ['A', 'T', 'G', 'C', 'T', 'A', 'G', 'C', 'G', 'A', 'T', 'C', 'C', 'A', 'T', 'G'], 'Strelka_0.5': [AA2, AT2, AG2, AC2, TT2, TA2, TG2, TC2, GG2, GA2, GT2, GC2, CC2, CA2, CT2, CG2]}
+data2 = {'REF': ['A', 'A', 'A', 'A', 'T', 'T', 'T', 'T', 'G', 'G', 'G', 'G', 'C', 'C', 'C', 'C'], 'ALT': ['A', 'T', 'G', 'C', 'T', 'A', 'G', 'C', 'G', 'A', 'T', 'C', 'C', 'A', 'T', 'G'], 'Strelka_0.7': [AA3, AT3, AG3, AC3, TT3, TA3, TG3, TC3, GG3, GA3, GT3, GC3, CC3, CA3, CT3, CG3]}
+
+# Collecting it into a dataframe.
+df = pd.DataFrame(data)
+df1 = pd.DataFrame(data1)
+df2 = pd.DataFrame(data2)
+
+# Merging columns based on "CHROM-POS"
+First = pd.merge(df, df1, on=['ALT', 'REF'])
+Second = pd.merge(First, df2, on=['ALT', 'REF'])
+
+# Mentioning the column names and inputing the csv file.
+Second.columns = ['REF', 'ALT', 'Strelka_0.3', 'Strelka_0.5', 'Strelka_0.7']
+print(Second)
+
+# Saving the results in csv.
+Second.to_csv('Strelka_Counts.csv', sep=',', index = None)
+
+# Reading csv files and concatinating "CHROM" and "POS"
+dff = pd.read_csv("Strelka_Counts.csv", sep = ',', index_col= False, error_bad_lines=False)
+dff.columns = ['REF', 'ALT', 'Strelka_One', 'Strelka_Two', 'Strelka_Three']
+
+# set width of bar
+width = 0.25
+
+# Columns from the file
+a1 = dff.Strelka_One.to_list()
+a2 = dff.Strelka_Two.to_list()
+a3 = dff.Strelka_Three.to_list()
+
+# Set position of bar on X axis
+r1 = np.arange(len(a1))
+r2 = [x + width for x in r1]
+r3 = [x + width for x in r2]
+
+# Make the plot
+plt.bar(r1, a1, color='#FFD700', width=width, edgecolor='white', label='Strelka_0.3')
+plt.bar(r2, a2, color='#FFA500', width=width, edgecolor='white', label='Strelka_0.5')
+plt.bar(r3, a3, color='#DC143C', width=width, edgecolor='white', label='Strelka_0.7')
+
+# Add xticks on the middle of the group bars
+plt.xlabel('Combinations')
+plt.xticks([r + width for r in range(len(a1))], ['AA', 'AT', 'AG', 'AC', 'TT', 'TA', 'TG', 'TC', 'GG', 'GA', 'GT', 'GC', 'CC', 'CA', 'CT', 'CG'])
+
+# Create legend & Show graphic
+plt.legend()
+plt.show()
+plt.savefig('Strelka_Counts_Plot.pdf')
+plt.savefig('Strelka_Counts_Plot.png', dpi = 300)
