df3b8e085603f70007ee29f8a08aa1868f8b7611
cvravi9 <cvravi9@gmail.com>
2021-04-25 22:45:49 +0200

    Adding modified results.

--- Read_Depth/Strelka/Background_Files/Code.py
+++ Read_Depth/Strelka/Background_Files/Code.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 import csv
 
 # Reading the csv input file that is obtained after performing the following operations on the vcf file.
-# Step 1 - 'cut -f 1-2, 9-11 Input-VCF-File > Output-VCF-File'
+# Step 1 - 'cut -f 1-2,9-11 Input-VCF-File > Output-VCF-File'
 # Step 2 - 'sed '/^#/d' Output-VCF-File > Updated_Output-VCF-File'
 
 # The first step is to selected the needed columns in the vcf file.
@@ -21,9 +21,9 @@ dff1 = pd.read_csv("Updated_Strelka_0.5.vcf", sep = '\t', index_col= False)
 dff2 = pd.read_csv("Updated_Strelka_0.7.vcf", sep = '\t', index_col= False)
 
 # Naming the columns after importing the csv file.
-dff.columns = ['CHROM', 'POS', 'NORMAL', 'TUMOR', '2:NORMAL', '2:TUMOR']
-dff1.columns = ['CHROM', 'POS', 'NORMAL', 'TUMOR', '2:NORMAL', '2:TUMOR']
-dff2.columns = ['CHROM', 'POS', 'NORMAL', 'TUMOR', '2:NORMAL', '2:TUMOR']
+dff.columns = ['CHROM', 'POS', 'FORMAT', 'NORMAL', 'TUMOR']
+dff1.columns = ['CHROM', 'POS', 'FORMAT', 'NORMAL', 'TUMOR']
+dff2.columns = ['CHROM', 'POS', 'FORMAT', 'NORMAL', 'TUMOR']
 
 # Concatinating the "CHROM" and "POS"
 dff["CHROM_POS"] = dff['CHROM'].astype(str) + '-' + dff['POS'].astype(str)
@@ -52,25 +52,19 @@ print(dff2)
 # Creating new columns by splitting the "NORMAL" and "TUMOR" columns by ':' and renaming the new columns based on the format "DP:FDP:SDP:SUBDP:AU:CU:GU:TU"
 dff[['Normal_Read_Depth', 'NORMAL-FDP', 'NORMAL-SDP', 'NORMAL-SUBDP', 'NORMAL-AU', 'NORMAL-CU', 'NORMAL-GU', 'NORMAL-TU', 'NORMAL-Last']] = dff['NORMAL'].str.split(':',expand=True)
 dff[['Tumor_Read_Depth', 'TUMOR-FDP', 'TUMOR-SDP', 'TUMOR-SUBDP', 'TUMOR-AU', 'TUMOR-CU', 'TUMOR-GU', 'TUMOR-TU', 'TUMOR-Last']] = dff['TUMOR'].str.split(':',expand=True)
-dff[['2:Normal_Read_Depth', 'NORMAL1-FDP', 'NORMAL1-SDP', 'NORMAL1-SUBDP', 'NORMAL1-AU', 'NORMAL1-CU', 'NORMAL1-GU', 'NORMAL1-TU', 'NORMAL1-Last']] = dff['2:NORMAL'].str.split(':',expand=True)
-dff[['2:Tumor_Read_Depth', 'TUMOR1-FDP', 'TUMOR1-SDP', 'TUMOR1-SUBDP', 'TUMOR1-AU', 'TUMOR1-CU', 'TUMOR1-GU', 'TUMOR1-TU', 'TUMOR1-Last']] = dff['2:TUMOR'].str.split(':',expand=True)
 
 dff1[['Normal_Read_Depth', 'NORMAL-FDP', 'NORMAL-SDP', 'NORMAL-SUBDP', 'NORMAL-AU', 'NORMAL-CU', 'NORMAL-GU', 'NORMAL-TU', 'NORMAL-Last']] = dff1['NORMAL'].str.split(':',expand=True)
 dff1[['Tumor_Read_Depth', 'TUMOR-FDP', 'TUMOR-SDP', 'TUMOR-SUBDP', 'TUMOR-AU', 'TUMOR-CU', 'TUMOR-GU', 'TUMOR-TU', 'TUMOR-Last']] = dff1['TUMOR'].str.split(':',expand=True)
-dff1[['2:Normal_Read_Depth', 'NORMAL1-FDP', 'NORMAL1-SDP', 'NORMAL1-SUBDP', 'NORMAL1-AU', 'NORMAL1-CU', 'NORMAL1-GU', 'NORMAL1-TU', 'NORMAL1-Last']] = dff1['2:NORMAL'].str.split(':',expand=True)
-dff1[['2:Tumor_Read_Depth', 'TUMOR1-FDP', 'TUMOR1-SDP', 'TUMOR1-SUBDP', 'TUMOR1-AU', 'TUMOR1-CU', 'TUMOR1-GU', 'TUMOR1-TU', 'TUMOR1-Last']] = dff1['2:TUMOR'].str.split(':',expand=True)
 
 dff2[['Normal_Read_Depth', 'NORMAL-FDP', 'NORMAL-SDP', 'NORMAL-SUBDP', 'NORMAL-AU', 'NORMAL-CU', 'NORMAL-GU', 'NORMAL-TU', 'NORMAL-Last']] = dff2['NORMAL'].str.split(':',expand=True)
 dff2[['Tumor_Read_Depth', 'TUMOR-FDP', 'TUMOR-SDP', 'TUMOR-SUBDP', 'TUMOR-AU', 'TUMOR-CU', 'TUMOR-GU', 'TUMOR-TU', 'TUMOR-Last']] = dff2['TUMOR'].str.split(':',expand=True)
-dff2[['2:Normal_Read_Depth', 'NORMAL1-FDP', 'NORMAL1-SDP', 'NORMAL1-SUBDP', 'NORMAL1-AU', 'NORMAL1-CU', 'NORMAL1-GU', 'NORMAL1-TU', 'NORMAL1-Last']] = dff2['2:NORMAL'].str.split(':',expand=True)
-dff2[['2:Tumor_Read_Depth', 'TUMOR1-FDP', 'TUMOR1-SDP', 'TUMOR1-SUBDP', 'TUMOR1-AU', 'TUMOR1-CU', 'TUMOR1-GU', 'TUMOR1-TU', 'TUMOR1-Last']] = dff2['2:TUMOR'].str.split(':',expand=True)
 
 # Dropping of the unnecessary columns and only choosing the "NORMAL Depth" i.e. "NORMAL-DP" and "TUMOR Depth" i.e. "TUMOR-DP"
-dff = dff.drop(['NORMAL', 'TUMOR', '2:NORMAL', '2:TUMOR', 'NORMAL-FDP', 'NORMAL-SDP', 'NORMAL-SUBDP', 'NORMAL-AU', 'NORMAL-CU', 'NORMAL-GU', 'NORMAL-TU', 'NORMAL-Last', 'TUMOR-FDP', 'TUMOR-SDP', 'TUMOR-SUBDP', 'TUMOR-AU', 'TUMOR-CU', 'TUMOR-GU', 'TUMOR-TU', 'TUMOR-Last', 'NORMAL1-FDP', 'NORMAL1-SDP', 'NORMAL1-SUBDP', 'NORMAL1-AU', 'NORMAL1-CU', 'NORMAL1-GU', 'NORMAL1-TU', 'NORMAL1-Last', 'TUMOR1-FDP', 'TUMOR1-SDP', 'TUMOR1-SUBDP', 'TUMOR1-AU', 'TUMOR1-CU', 'TUMOR1-GU', 'TUMOR1-TU', 'TUMOR1-Last'], axis=1)
+dff = dff.drop(['NORMAL', 'TUMOR', 'NORMAL-FDP', 'NORMAL-SDP', 'NORMAL-SUBDP', 'NORMAL-AU', 'NORMAL-CU', 'NORMAL-GU', 'NORMAL-TU', 'NORMAL-Last', 'TUMOR-FDP', 'TUMOR-SDP', 'TUMOR-SUBDP', 'TUMOR-AU', 'TUMOR-CU', 'TUMOR-GU', 'TUMOR-TU', 'TUMOR-Last'], axis=1)
 
-dff1 = dff1.drop(['NORMAL', 'TUMOR', '2:NORMAL', '2:TUMOR', 'NORMAL-FDP', 'NORMAL-SDP', 'NORMAL-SUBDP', 'NORMAL-AU', 'NORMAL-CU', 'NORMAL-GU', 'NORMAL-TU', 'NORMAL-Last', 'TUMOR-FDP', 'TUMOR-SDP', 'TUMOR-SUBDP', 'TUMOR-AU', 'TUMOR-CU', 'TUMOR-GU', 'TUMOR-TU', 'TUMOR-Last', 'NORMAL1-FDP', 'NORMAL1-SDP', 'NORMAL1-SUBDP', 'NORMAL1-AU', 'NORMAL1-CU', 'NORMAL1-GU', 'NORMAL1-TU', 'NORMAL1-Last', 'TUMOR1-FDP', 'TUMOR1-SDP', 'TUMOR1-SUBDP', 'TUMOR1-AU', 'TUMOR1-CU', 'TUMOR1-GU', 'TUMOR1-TU', 'TUMOR1-Last'], axis=1)
+dff1 = dff1.drop(['NORMAL', 'TUMOR', 'NORMAL-FDP', 'NORMAL-SDP', 'NORMAL-SUBDP', 'NORMAL-AU', 'NORMAL-CU', 'NORMAL-GU', 'NORMAL-TU', 'NORMAL-Last', 'TUMOR-FDP', 'TUMOR-SDP', 'TUMOR-SUBDP', 'TUMOR-AU', 'TUMOR-CU', 'TUMOR-GU', 'TUMOR-TU', 'TUMOR-Last'], axis=1)
 
-dff2 = dff2.drop(['NORMAL', 'TUMOR', '2:NORMAL', '2:TUMOR', 'NORMAL-FDP', 'NORMAL-SDP', 'NORMAL-SUBDP', 'NORMAL-AU', 'NORMAL-CU', 'NORMAL-GU', 'NORMAL-TU', 'NORMAL-Last', 'TUMOR-FDP', 'TUMOR-SDP', 'TUMOR-SUBDP', 'TUMOR-AU', 'TUMOR-CU', 'TUMOR-GU', 'TUMOR-TU', 'TUMOR-Last', 'NORMAL1-FDP', 'NORMAL1-SDP', 'NORMAL1-SUBDP', 'NORMAL1-AU', 'NORMAL1-CU', 'NORMAL1-GU', 'NORMAL1-TU', 'NORMAL1-Last', 'TUMOR1-FDP', 'TUMOR1-SDP', 'TUMOR1-SUBDP', 'TUMOR1-AU', 'TUMOR1-CU', 'TUMOR1-GU', 'TUMOR1-TU', 'TUMOR1-Last'], axis=1)
+dff2 = dff2.drop(['NORMAL', 'TUMOR', 'NORMAL-FDP', 'NORMAL-SDP', 'NORMAL-SUBDP', 'NORMAL-AU', 'NORMAL-CU', 'NORMAL-GU', 'NORMAL-TU', 'NORMAL-Last', 'TUMOR-FDP', 'TUMOR-SDP', 'TUMOR-SUBDP', 'TUMOR-AU', 'TUMOR-CU', 'TUMOR-GU', 'TUMOR-TU', 'TUMOR-Last'], axis=1)
 
 # Replacing '.' values with '0'
 dff.replace('.', '0', inplace=True)
@@ -79,40 +73,27 @@ dff2.replace('.', '0', inplace=True)
 
 # Converting string values columans to int.
 dff['Normal_Read_Depth'] = dff['Normal_Read_Depth'].astype(int)
-dff['2:Normal_Read_Depth'] = dff['2:Normal_Read_Depth'].astype(int)
 dff['Tumor_Read_Depth'] = dff['Tumor_Read_Depth'].astype(int)
-dff['2:Tumor_Read_Depth'] = dff['2:Tumor_Read_Depth'].astype(int)
 
 dff1['Normal_Read_Depth'] = dff1['Normal_Read_Depth'].astype(int)
-dff1['2:Normal_Read_Depth'] = dff1['2:Normal_Read_Depth'].astype(int)
 dff1['Tumor_Read_Depth'] = dff1['Tumor_Read_Depth'].astype(int)
-dff1['2:Tumor_Read_Depth'] = dff1['2:Tumor_Read_Depth'].astype(int)
 
 dff2['Normal_Read_Depth'] = dff2['Normal_Read_Depth'].astype(int)
-dff2['2:Normal_Read_Depth'] = dff2['2:Normal_Read_Depth'].astype(int)
 dff2['Tumor_Read_Depth'] = dff2['Tumor_Read_Depth'].astype(int)
-dff2['2:Tumor_Read_Depth'] = dff2['2:Tumor_Read_Depth'].astype(int)
-
-# Adding columns for single read depth value.
-dff['Normal_RD'] = dff["Normal_Read_Depth"] + dff["2:Normal_Read_Depth"]
-dff['Tumor_RD'] = dff["Tumor_Read_Depth"] + dff["2:Tumor_Read_Depth"]
-
-dff1['Normal_RD'] = dff1["Normal_Read_Depth"] + dff1["2:Normal_Read_Depth"]
-dff1['Tumor_RD'] = dff1["Tumor_Read_Depth"] + dff1["2:Tumor_Read_Depth"]
-
-dff2['Normal_RD'] = dff2["Normal_Read_Depth"] + dff2["2:Normal_Read_Depth"]
-dff2['Tumor_RD'] = dff2["Tumor_Read_Depth"] + dff2["2:Tumor_Read_Depth"]
+print(dff)
+print(dff1)
+print(dff2)
 
 # Dropping of the unnecessary columns and reorganising the columns.
-dff = dff.drop(['Normal_Read_Depth', 'Tumor_Read_Depth', '2:Normal_Read_Depth', '2:Tumor_Read_Depth'], axis=1)
+dff = dff.drop(['FORMAT'], axis=1)
 dff.columns = ['CHROM_POS', 'Normal_Read_Depth', 'Tumor_Read_Depth']
 print(dff)
 
-dff1 = dff1.drop(['Normal_Read_Depth', 'Tumor_Read_Depth', '2:Normal_Read_Depth', '2:Tumor_Read_Depth'], axis=1)
+dff1 = dff1.drop(['FORMAT'], axis=1)
 dff1.columns = ['CHROM_POS', 'Normal_Read_Depth', 'Tumor_Read_Depth']
 print(dff1)
 
-dff2 = dff2.drop(['Normal_Read_Depth', 'Tumor_Read_Depth', '2:Normal_Read_Depth', '2:Tumor_Read_Depth'], axis=1)
+dff2 = dff2.drop(['FORMAT'], axis=1)
 dff2.columns = ['CHROM_POS', 'Normal_Read_Depth', 'Tumor_Read_Depth']
 print(dff2)
 
